3bcf837aed17155f997a6bac5021abc6a8260640
Yashna Kumar <yashna.kumar@gmail.com>
2015-08-13 16:51:47 -0700

    Pre fixing constraints

---
+++ Makestagram/ViewControllers/SettingsViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SettingsViewController.swift
+//  Makestagram
+//
+//  Created by Rajesh on 8/13/15.
+//  Copyright (c) 2015 Make School. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+//    @IBAction func cancelButtonPressed(sender: AnyObject) {
+//        self.dismissViewControllerAnimated(true, completion: {});
+//    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.dismissViewControllerAnimated(true, completion: {});
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
